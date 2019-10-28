f89d0b69cabf3ab574be09f0868945e8ff4ff187
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2019-10-28 14:02:39 -0400

    Alterado diretório da rotas e removido rotas de api

--- src/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
-    Route::get('galleries/images', ['uses' => 'ApiController@images', 'as' => 'api.galleries.images']);
-});
